9c44394f7842e5a2283afc81290def2fffdf1c0a
Vicent Gilabert <gilabert_vicent@hotmail.com>
2024-10-02 23:03:07 +0200

    clean repo

--- server/django_server/urls.py
+++ server/django_server/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('usuario/', include('usuario.urls')),
+    path('core/', include('core.urls')),
     #path('intercambios/', include('intercambios.urls')),
-
 ]
